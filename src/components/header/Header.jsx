006dcc5f7049314429cf8474d99021d194fb6171
Burak Ustun <burakustun54@hotmail.com>
2023-02-23 12:32:47 +0300

    start

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import TaskPng from "../../assets/TaskPng"
+
+
+
+const Header = () => {
+  return (
+    <div className="d-flex justify-content-evenly align-items-center bg-danger">
+      <TaskPng/>
+      <h1>TASK TRAKER</h1>
+    </div>
+  )
+}
+
+export default Header
