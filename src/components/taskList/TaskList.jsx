c34d308b1865fd9a239b4ca1e1198fe361876eef
Burak Ustun <burakustun54@hotmail.com>
2023-02-23 15:42:30 +0300

    v4

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,11 +1,35 @@
-
+import {RiDeleteBack2Fill} from "react-icons"
 
 const TaskList = ({task, getTask}) => {
 
-  
+  const deleteTask =()=>{
+    try {
+      
+    } catch (error) {
+      
+    }
+
+  }
+
+
   return (
     <div>
-      
+      {task.map((item)=>{
+        const {id,task,date}=item
+        return (
+          <div>
+            <div>
+              <h4>{task}</h4>
+              <p>{date}</p>
+            </div>
+              <div>
+                <RiDeleteBcak2Fill style={{cursor:"pointer"}}></RiDeleteBcak2Fill>
+              </div>
+          </div>
+
+
+        )
+      })}
     </div>
   )
 }
